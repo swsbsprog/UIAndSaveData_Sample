dcd3df625a86f4775d70f27bf1e1fb9b62bbe2ef
hktrollgames <hktrollgames@gmail.com>
2022-08-07 18:12:50 +0900

    C:\UnityProject\UIAndSaveData_Sample\Assets\SaveData.cs Add C:\UnityProject\UIAndSaveData_Sample\Assets\UserDataManager.cs Add

---
+++ Assets/SaveData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaveData : MonoBehaviour
+{
+    public UserData userData;
+
+    void Awake()
+    {
+        string json = PlayerPrefs.GetString(UserData.Key);
+        if (!string.IsNullOrEmpty(json))
+        {
+            userData = JsonUtility.FromJson<UserData>(json);
+            print($"데이터 읽음 {userData.version}");
+        }
+        else
+            print($"데이터 없음 {userData.version}");
+    }
+
+    // 에디터에서 사용, 휴대폰에서 작동안함(프로세스가 죽어서 저장 못함)
+    private void OnDestroy() => SaveSerializeData();
+
+    // 휴대폰에서 사용.(앱 끄기 위해서 비활성화 할때 저장)
+    // pauseStatus == true -> 앱이 멈췄다
+    void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus)
+            SaveSerializeData();
+    }
+
+    private void SaveSerializeData()
+    {
+        string json = JsonUtility.ToJson(userData);
+        PlayerPrefs.SetString(UserData.Key, json);
+        PlayerPrefs.Save();
+    }
+}
+
+[Serializable]
+public class UserData
+{
+    public const string Key = nameof(UserData);
+    public int version = 1;
+    public int gold;
+    public int score;
+    public bool useSound;
+    public List<string> haveItem = new ();
+}
---
+++ Assets/UserDataManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UserDataManager : MonoBehaviour
+{
+    public TMP_Text goldText;
+    UserData userData;
+    public UserData UserData
+    {
+        get
+        {
+            if (userData == null)
+                userData = GetComponent<SaveData>().userData;
+            return userData;
+        }
+    }
+
+    private void UpdateGoldUI()
+    {
+        //숫자 천단위로 ","표시됨 (예:1,000)
+        goldText.text = UserData.gold.ToString("N0"); 
+    }
+
+    private void Start() => UpdateGoldUI();
+    public void ChangeGold(int changeAmount)
+    {
+        UserData.gold += changeAmount;
+        UpdateGoldUI();
+    }
+
+    public void BuyItem(string buyItemName)
+    {
+        UserData.haveItem.Add(buyItemName);
+        ChangeGold(-10);
+    }
+    public void SellItem(string sellItemName)
+    {
+        UserData.haveItem.Remove(sellItemName);
+        ChangeGold(5);
+    }
+}
