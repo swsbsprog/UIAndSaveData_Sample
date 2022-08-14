c35f4053676d587b4d1b948737b2ce424d23a9f9
chanseon.park <chanseon.park@kongstudios.net>
2022-08-14 10:36:03 +0900

    구글 스프레드 시트 읽기 샘플 추가

---
+++ Assets/DataManager.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public List<GameItem> gameItems = new List<GameItem>();
+    void Start()
+    {
+        var jsonTextAsset = Resources.Load("JsonData/GameItem") as TextAsset;
+        var result = JsonConvert.DeserializeObject<List<GameItem>>(jsonTextAsset.text);
+        print(result[0].Name);
+    }
+}
+
+[System.Serializable]
+public class GameItem
+{
+    public int ID;
+    public string Name;
+    public int Price;
+}
